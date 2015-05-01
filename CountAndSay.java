33301054c8468e2c06a71670c5eb34a17402a99d
Abhishek Choudhary <abhishek.create@gmail.com>
2015-05-01 19:25:47 +0100

    Count and Say like 112 gives 2112

---
+++ CountAndSay.java
@@ -0,0 +1,30 @@
+
+public class CountAndSay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String countAndSay = new CountAndSay().countAndSay(1);
+		System.out.println(countAndSay);
+	}
+
+	
+	
+	 public String countAndSay(int n) {
+	        String output = "";
+	        int sum = 0;
+	        char[] charArray = (""+n).toCharArray();
+	        char prev = charArray[0];
+	        for (int i = 0; i < charArray.length; i++) {
+				if(charArray[i] == prev){
+					sum++;
+				}else{
+					output += ""+sum+""+prev;
+					prev = charArray[i];
+					sum = 1;
+				}
+				
+			}
+	        output += ""+sum+""+prev;
+	        return output;
+	    }
+}
