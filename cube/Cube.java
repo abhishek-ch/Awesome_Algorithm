b6dae950cba845b31d88dc6ea7a129e7c1a43a78
Abhishek Choudhary <abhishek.create@gmail.com>
2015-07-14 20:59:13 +0100

    Update Cube

---
+++ cube/Cube.java
@@ -0,0 +1,31 @@
+package cube;
+
+public class Cube {
+	
+	int[][] element = new int[5][5];
+	
+	public int[] getLeft(){
+		int[] left = new int[element.length];
+		for (int i = 0; i < element.length; i++) {
+			left[i] = element[i][0];
+		}
+		return left;
+	}
+	
+	
+	public int[] getRight(){
+		int[] right = new int[element.length];
+		for (int i = 0; i < element.length; i++) {
+			right[i] = element[i][4];
+		}
+		return right;
+	}
+	
+	public int[] getBottom(){
+		return element[4];
+	}
+	
+	public int[] getTop(){
+		return element[0];
+	}
+}
