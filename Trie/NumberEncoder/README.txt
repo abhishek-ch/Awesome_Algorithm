06b022fffdf8a1120682347376670dcf08d45ca2
Abhishek Choudhary <abhishek.create@gmail.com>
2015-07-17 09:27:33 +0100

    Updated with test case and Readme

---
+++ Trie/NumberEncoder/README.txt
@@ -0,0 +1,26 @@
+Go to package com.numberencoding.main
+
+Run The File MainFunction.java
+It will show you following in the console-
+Enter the path of dictionary file or 0 to exit: 
+	(enter the path of dictionary file)
+
+Enter the path of Input file to process or 0 to exit : 
+	(enter the path of input file)
+		
+	
+Sample:
+
+Enter the path of dictionary file or 0 to exit: 
+dictionary.txt
+Enter the path of Input file to process or 0 to exit : 
+input.txt
+
+38- : so
+10 : je
+7-3593-50 : 7 da 9 da 0
+-11/0 : 1 je
+/93-8- : 9 so
+
+The entire input file process is in loop , so once a dictionary is loaded you can keep adding different input files 
+for testing unless user doesn't enter 0.	
--- Trie/NumberEncoder/src/main/java/com/numberencoding/CharacterMapper.java
+++ Trie/NumberEncoder/src/main/java/com/numberencoding/CharacterMapper.java
@@ -3,46 +3,57 @@ package com.numberencoding;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CharacterMapper {
+/**
+ * 
+ * This class is responsible for mapping each Charcater representation to the number
+ * & store everything in a HashTable based datastructure (HashMap used here)
+ * 
+ * @author abc
+ * 
+ */
+public class CharacterMapper
+{
 
-	public Map<Character, Integer> buildCharacterMapper(){
-		Map<Character, Integer> charTOIntMap = new HashMap<Character, Integer>();
-		charTOIntMap.put('E', 0);
 
-		charTOIntMap.put('J', 1);
-		charTOIntMap.put('N', 1);
-		charTOIntMap.put('Q', 1);
+   public Map<Character, Integer> buildCharacterMapper()
+   {
+      Map<Character, Integer> charTOIntMap = new HashMap<Character, Integer>();
+      charTOIntMap.put('E', 0);
 
-		charTOIntMap.put('R', 2);
-		charTOIntMap.put('W', 2);
-		charTOIntMap.put('X', 2);
+      charTOIntMap.put('J', 1);
+      charTOIntMap.put('N', 1);
+      charTOIntMap.put('Q', 1);
 
-		charTOIntMap.put('D', 3);
-		charTOIntMap.put('S', 3);
-		charTOIntMap.put('Y', 3);
+      charTOIntMap.put('R', 2);
+      charTOIntMap.put('W', 2);
+      charTOIntMap.put('X', 2);
 
-		charTOIntMap.put('F', 4);
-		charTOIntMap.put('T', 4);
+      charTOIntMap.put('D', 3);
+      charTOIntMap.put('S', 3);
+      charTOIntMap.put('Y', 3);
 
-		charTOIntMap.put('A', 5);
-		charTOIntMap.put('M', 5);
+      charTOIntMap.put('F', 4);
+      charTOIntMap.put('T', 4);
 
-		charTOIntMap.put('C', 6);
-		charTOIntMap.put('I', 6);
-		charTOIntMap.put('V', 6);
+      charTOIntMap.put('A', 5);
+      charTOIntMap.put('M', 5);
 
-		charTOIntMap.put('B', 7);
-		charTOIntMap.put('K', 7);
-		charTOIntMap.put('U', 7);
+      charTOIntMap.put('C', 6);
+      charTOIntMap.put('I', 6);
+      charTOIntMap.put('V', 6);
 
-		charTOIntMap.put('L', 8);
-		charTOIntMap.put('O', 8);
-		charTOIntMap.put('P', 8);
+      charTOIntMap.put('B', 7);
+      charTOIntMap.put('K', 7);
+      charTOIntMap.put('U', 7);
 
-		charTOIntMap.put('G', 9);
-		charTOIntMap.put('H', 9);
-		charTOIntMap.put('Z', 9);
-		return charTOIntMap;
-	}
-	
+      charTOIntMap.put('L', 8);
+      charTOIntMap.put('O', 8);
+      charTOIntMap.put('P', 8);
+
+      charTOIntMap.put('G', 9);
+      charTOIntMap.put('H', 9);
+      charTOIntMap.put('Z', 9);
+      return charTOIntMap;
+   }
+
 }
