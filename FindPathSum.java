6ba3023a1428136d2cc7ddf92a9ad0c29b4dd6e2
Abhishek Choudhary <abhishek.create@gmail.com>
2015-10-24 20:02:39 +0100

    removed comments

--- FindPathSum.java
+++ FindPathSum.java
@@ -12,9 +12,3 @@ public class FindPathSum {
 
 }
 
-/*
- * Copyright 2004-2015 Pilz Ireland Industrial Automation Ltd. All Rights
- * Reserved. PILZ PROPRIETARY/CONFIDENTIAL.
- * 
- * Created on 17 Aug 2015
- */
